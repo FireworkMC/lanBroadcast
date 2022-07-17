5449fd4d8ceb621b6c4a949b073d7dd3131ed8cd
yehan2002 <yehanjaya2002@gmail.com>
2022-07-17 11:18:51 +0530

    Code cleanup.

--- lan.go
+++ lan.go
@@ -19,46 +19,54 @@ const (
 	ErrHost = errors.Error("lanBroadcast: unable to get the host address or interface")
 )
 
-//the default port and address used by vanila minecraft
+// the default port and address used by vanilla minecraft
 const broadcastHost = "224.0.2.60:4445"
 
-//LANBroadcast a lan broadcaster
+// LANBroadcast a lan broadcaster
 type LANBroadcast struct {
 	sender        net.PacketConn
 	broadcastAddr *net.UDPAddr
 	buffer        bytes.Buffer
 	interval      time.Duration
 
-	ctx    context.Context
-	cancel context.CancelFunc
+	cancelMux sync.Mutex
+	cancel    context.CancelFunc
 
 	mux  sync.Mutex
 	port int
 	motd string
 }
 
-//SetMOTD set the message of the day
+// SetMOTD set the message of the day
 func (l *LANBroadcast) SetMOTD(message string) {
 	l.mux.Lock()
 	l.motd = message
 	l.mux.Unlock()
 }
 
-//SetInterval set the interval between broadcasts in seconds.
-//This has no effect if called after calling `Broadcast()`.
+// SetInterval set the interval between broadcasts in seconds.
+// This has no effect if called after calling `Broadcast()`.
 func (l *LANBroadcast) SetInterval(t uint) {
 	l.mux.Lock()
 	l.interval = time.Second * time.Duration(t)
 	l.mux.Unlock()
 }
 
-//Broadcast broadcasts the game to LAN.
-//This function blocks until the given ctx is canceled.
-func (l *LANBroadcast) Broadcast() {
+// Broadcast broadcasts the game to LAN.
+// This function blocks until the given ctx is canceled or Close is called.
+func (l *LANBroadcast) Broadcast(ctx context.Context) {
 	var interval = time.Second * 5
 	if l.interval != 0 {
 		interval = l.interval
 	}
+
+	l.cancelMux.Lock()
+	if l.cancel != nil {
+		panic("Tried to start multiple broadcasts")
+	}
+	ctx, l.cancel = context.WithCancel(ctx)
+	l.cancelMux.Unlock()
+
 	tick := time.Tick(interval)
 	for {
 		select {
@@ -66,16 +74,23 @@ func (l *LANBroadcast) Broadcast() {
 			if err := l.sendPacket(); err != nil {
 				fmt.Println("Error sending LAN broadcast:", err)
 			}
-		case <-l.ctx.Done():
+		case <-ctx.Done():
 			l.sender.Close()
 			return
 		}
 	}
 }
 
-//Close close the LAN broadcaster.
-//This function does not block and may be called more than once.
-func (l *LANBroadcast) Close() { l.cancel() }
+// Close close the LAN broadcaster.
+// This function does not block and may be called more than once.
+func (l *LANBroadcast) Close() {
+	l.cancelMux.Lock()
+	if l.cancel != nil {
+		l.cancel()
+		l.cancel = nil
+	}
+	l.cancelMux.Unlock()
+}
 
 func (l *LANBroadcast) sendPacket() error {
 	l.mux.Lock()
@@ -89,8 +104,8 @@ func (l *LANBroadcast) sendPacket() error {
 	return err
 }
 
-//NewLANBroadcast creates a new lan broadcaster
-func NewLANBroadcast(ctx context.Context, ip net.IP, port int, motd string) (l *LANBroadcast, err error) {
+// New creates a new lan broadcaster
+func New(ip net.IP, port int, motd string) (l *LANBroadcast, err error) {
 	if port <= 0 { // disallow negative ports and port 0 since it is not a real port
 		return nil, ErrInvalidPort
 	}
@@ -101,8 +116,7 @@ func NewLANBroadcast(ctx context.Context, ip net.IP, port int, motd string) (l *
 		}
 	}
 
-	ctx, cancel := context.WithCancel(ctx)
-	lan := LANBroadcast{cancel: cancel, ctx: ctx, port: port, motd: motd}
+	lan := LANBroadcast{port: port, motd: motd}
 	if lan.sender, err = net.ListenPacket("udp", ip.String()+":"); err == nil {
 		if lan.broadcastAddr, err = net.ResolveUDPAddr("udp", broadcastHost); err == nil {
 			l = &lan
